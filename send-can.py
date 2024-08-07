b6868336900408f4e5931e95a462080c597882fe
flavin-jr <fjcvj@cin.ufpe.br>
2024-08-06 21:17:05 -0300

    feat: add basic send code

---
+++ send-can.py
@@ -0,0 +1,35 @@
+import can
+import time
+
+def parse_can_message(line):
+    # Remove os par  nteses e divida a linha por espa  os
+    parts = line.strip().split()
+    timestamp = float(parts[0][1:-1])
+    can_interface = parts[1]
+    can_id_data = parts[2].split('#')
+    can_id = int(can_id_data[0], 16)
+    data = bytes.fromhex(can_id_data[1])
+    flag = parts[3]
+    return timestamp, can_interface, can_id, data, flag
+
+def send_can_messages(file_path, interface):
+    # Configurar a interface CAN
+  with can.interface.Bus(channel=interface, bustype='socketcan') as bus:
+
+    with open(file_path, 'r') as f:
+      for line in f:
+        timestamp, can_interface, can_id, data, flag = parse_can_message(line)
+        message = can.Message(arbitration_id=can_id, data=data, is_extended_id=False)
+        try:
+          flag = "NORMAL" if flag == 'R' else "ATAQUE"
+          bus.send(message)
+          print(f"Message sent on {can_interface}: {message} LABEL = {flag}")
+        except can.CanError:
+          print("Message NOT sent")
+        time.sleep(0.5)
+
+if __name__ == "__main__":
+    file_path = 'candump-2024-07-03_161118.log'  # Substitua pelo caminho do seu arquivo de mensagens
+    interface = 'can0'  # Substitua pela sua interface CAN
+
+    send_can_messages(file_path, interface)
