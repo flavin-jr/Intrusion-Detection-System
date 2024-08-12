b1f295542d6f5bc41a18a3aea45a13f079454bfe
flavin-jr <fjcvj@cin.ufpe.br>
2024-08-12 02:46:40 -0300

    feat: update receive-can.py

--- receive-can.py
+++ receive-can.py
@@ -1,4 +1,10 @@
+
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
 from can import Message
+import joblib
 import can
 def test_index(mensagem):
   can_message_map=[0x007,0x008,0x00D,0x00E,0x014,0x015,0x016,0x017,0x041,0x055,0x056,0x05B,0x05C,0x05D]
@@ -18,32 +24,35 @@ def filtro_DBC(mensagem): #retorna True se for ataque direto. Se n  o, retorna F
   else:
     return False
 
+def process_can_msg(message):
+  processed_msg = []
+  id=message.arbitration_id
+  dlc=message.dlc
+  data=[message.data[i] for i in range(0, dlc)] #message.data    do tipo bytes j   separado em bytes!!!
+  data += [0] * (8 - len(data))
+  processed_msg.append(id)
+  processed_msg.extend(data)
+  return processed_msg
 
 def read_from_can():
   interface = 'socketcan'
   channel = 'can0'
-  naoDetectados=[]
   try:
 
+      model = joblib.load('random_forest_classifier.pkl')
       while True:
         with can.Bus(interface=interface,channel=channel) as bus:
           message = bus.recv()
           if(filtro_DBC(message)):
             print("ATAQUE DETECTADO!\n")
           else:
-      #tratar o dado pra entrada do modelo
-#      id=message.arbitration_id
- #     dlc=message.dlc
-  #    data=[int(message.data[i], 16) for i in range(0, len(message.data))] #message.data    do tipo bytes j   separado em bytes!!!
-   #   data += [0] * (8 - len(data))
-    #  naoDetectados.append([id,dlc]+ data) #ou model.predict( ([id,dlc]+ data) ) considerando a entrada do modelo um array
-      #nessa linha desse comentario, seria verificado a predi    o do modelo, que responderia pra ataque ou n  o        print("MENSAGEM NORMAL")
-            print("MENSAGEM NORMAL\n")
-            print(int.from_bytes(message.data, byteorder='big'))
-  #aqui na pratica n sei oq daria pra p  r
- # df = pd.DataFrame(sequences,columns=['can_id','can_dlc','data0','data1','data2','data3','data4','data5','data6','data7'])
-  #df = codificar_dados(df) #o retorno daqui ta pronto pro 'predict' do modelo. e aqui tem 10 mensagens
- # return df
+          #tratamento de dados para o modelo
+            processed_msg = process_can_msg(message)
+            result = model.predict([processed_msg])
+            result = "MENSAGEM NORMAL\n" if result[0]==0 else "ATAQUE DETECTADO!\n"
+            print(result)
+
+
   except KeyboardInterrupt:
     print("\n Interrupted by user. Exiting...")
   finally:
